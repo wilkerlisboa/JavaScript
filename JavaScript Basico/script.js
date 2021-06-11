08a5e1e1b3c7d4805787201dbead12777ddb9720
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-06-11 10:52:57 -0300

    Inital commit

---
+++ JavaScript Basico/script.js
@@ -0,0 +1,21 @@
+// tudo que vem após barras normais duplas é um comentário em linguagem natural.
+// Leia os comentários atentamentes: eles explicam o codigo JavaScript.
+
+//Variável é um nome simbólico para um valor.
+//As variáveis são declaradas com a palavra - chave VAR:
+
+var x;  //Declara uma variável chamada 'x'.
+
+//Valores podem ser atribuidos às variáveis com o sinal '='
+
+x = 0; //Agora a variável 'x' tem o valor 0 
+
+x // => 0; Uma variável é avaliada com seu valor.
+
+//JavaScript aceita vários tipos de valores
+x = 1; //Números 
+x = 0.01; //Apenas um tipo Number para inteiros e reais.
+x = "hello world"; //Strings de texto entre aspas.
+x = 'JavaScript'; //Apóstrofos também delimitam strings.
+x = true; //Valores Booleanos.
+x = false; //Outro valor booleano. 
