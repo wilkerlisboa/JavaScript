ad78f877cc8c134bc09b3d31ed1c4f60079da11c
Wilker Lisboa <wilkerlisboa.ofc@gmail.com>
2021-06-29 16:16:41 -0300

    Initial commit

--- JavaScript Basico/teste.js
+++ JavaScript Basico/teste.js
@@ -21,5 +21,22 @@ document.write(name + '<br>');
 
 document.write(home + '<br>');
 
+//Array 
+
+var MyArray = new Array();
+
+MyArray[0] = 'JOÃO COSTA GOMES';
+MyArray[1] = 'SÃO PAULO';
+MyArray[2] = true;
+MyArray[3] = 18;
+
+document.write(MyArray[0] + "<br>");
+
+document.write(MyArray[1] + "<br>");
+
+document.write(MyArray[2] + "<br>");
+
+document.write(MyArray[3] + "<br>");
+    
 
 
